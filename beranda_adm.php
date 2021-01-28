fa75dde40ba4b438b83ac20f57b921af2ed8180f
sriayuni199 <78128213+sriayuni199@users.noreply.github.com>
2021-01-28 10:33:55 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,42 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator Sistem Informasi Peminjaman Buku Perpustakaan Kab.Asahan</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/asahanlogo1.png"></img></p>
+                          <p class="label-title" align="center"><strong>PERPUSTAKAAN DAERAH KAB.ASAHAN</strong></p>
+                          <p class="label-title" align="center"><strong>PUTRI AYU INDRAYANTI  17220234</strong></p>
+                          <p class="label-title" align="center"><strong>SRI AYU WIDIASTUTI  17220243</strong></p>
+                          <p class="label-title" align="center"><strong>SRI AYUNI 17220244</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Pemijman Buku Perpustakaan Kabupaten Asahan.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data buku, pengelolaan peminjaman buku, <br>
+                            dan juga pengelolaan laporan buku serta peminjamannya.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan data buku dan data peminjaman buku.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
